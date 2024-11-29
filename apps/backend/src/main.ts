86ca27df9553e83ffaa938addc941d938a03e9fe
Mark <m4rc0d3r@gmail.com>
2024-11-29 16:49:41 +0200

    Chage port for 'backend' app one more time

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5345);
+  await app.listen(3984);
 }
 bootstrap();
